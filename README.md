ad0b133cf4942b119bc12030b34521628db4644c
DesignerJennifer <54449493+DesignerJennifer@users.noreply.github.com>
2020-01-31 02:42:39 -0600

    updated color of field

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Work-Day-Planner
+# Day-Planner
+A daily planner that displays the current date and allows for input of information by the user that is saved, even after a refresh.
--- script.js
+++ script.js
@@ -1,65 +1,46 @@
 //Displays current date.
-var currentDay = moment().format("dddd, MMMM Do YYYY");
-console.log(currentDay)
+var currentDate = moment().format("dddd, MMMM Do YYYY");
 
+document.getElementById("currentDay").innerHTML = currentDate;
 //Displays current hour
 var currentHour = moment().format("HH");
 console.log(currentHour)
 
 var timeDisplay = ["nine", "ten", "eleven", "noon", "one", "two", "three", "for", "five"];
-var hourValues = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+var hourValues = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 1, 2, 3, 4, 5, 6, 7, 8];
 var hourBlocks = moment.duration(1, 'hours');
+
 
 function showPlanner() {
   for (let index = 0; index < hourValues.length; index++) {
     var row = $("<div class='row'>")
-
     var col1 = $("<div class='col-sm-2 text-right'>")
 
     col1.html(hourValues[index] + ":00 AM")
 
     if (hourValues[index] >= 12) {
       var hours = hourValues[index] - 12;
-      if(hours === 0) {
+      if (hours === 0) {
         (hours = 12)
       }
       col1.html(hours + ":00 PM")
-
     }
 
     var col2 = $("<div class='col-sm-8'>")
     var textarea = $("<textarea class='form-control'>")
     col2.append(textarea)
     var col3 = $("<div class='col-sm-2'>")
-    var saveBtn = $("<button class='btn btn-primary'>")
+    var saveBtn = $("<button class='btn btn-primary saveBtn'>")
     saveBtn.text("Save")
     col3.append(saveBtn)
     row.append(col1, col2, col3)
     $(".planner").append(row)
-
   }
-}
 
+}
 showPlanner()
 
-$("#currentDay").text(currentDay);
 
+  $("textarea.form-control").css("background-color","red");
 
-
-function timeDisplay() {
-  moment(hourValues).isBefore(currentDay)
-
-  timeDisplay = past
-
-
-  if (hourValues[i] === currentDay)
-    timeDisplay = current
-
-  if (hourValues[i] < currentDay)
-    timeDisplay = future
-}
-
-
-
-
-
+  
