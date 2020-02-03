d0d03b05f36f07faea37aab25388d485a80bc759
DesignerJennifer <54449493+DesignerJennifer@users.noreply.github.com>
2020-02-03 00:02:28 -0600

    got colors and local storage working

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-//Displays current date.
+//moment retrieves the current date from the system, and formats the date by day, month and year
 var currentDate = moment().format("dddd, MMMM Do YYYY");
+
+//takes the day, month and year data and displays it in the ID currentDay paragraph
 document.getElementById("currentDay").innerHTML = currentDate;
 
-//Displays current hour
-var currentHour = moment().format("HH");
-console.log(currentHour)
+//Current hour is genereated by calling the moment.js fuction that targets the current hour
+var currentHour = moment().hour();
+//console.log(currentHour)
 
-var timeDisplay = ["nine", "ten", "eleven", "noon", "one", "two", "three", "for", "five"];
-var hourValues = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 1, 2, 3, 4, 5, 6, 7, 8];
+//hourValues grabs the data as military time in order to create unique labels for the time
+var hourValues = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+
+//hourBlocks is telling us the length of time each input field on the planner represents.
 var hourBlocks = moment.duration(1, 'hours');
 
 //dynamically generated planner rows/columns
+
+//Creating the fuction that creates the day planner
 function showPlanner() {
+  //for loop that loop thorugh the hourvalues index
   for (let index = 0; index < hourValues.length; index++) {
+    //
     var row = $("<div class='row'>")
     var col1 = $("<div class='col-sm-2 text-right'>")
 
@@ -27,7 +35,26 @@ function showPlanner() {
     }
 
     var col2 = $("<div class='col-sm-8'>")
-    var textarea = $("<textarea class='form-control'>")
+
+    var   textarea = $("<textarea class='form-control'>")
+    
+    if(currentHour < hourValues[index]) {
+      textarea = $("<textarea class='form-control bg-success text-white'>")
+
+    }
+    else if(hourValues[index] === currentHour) {
+      textarea = $("<textarea class='form-control bg-danger text-white'>")
+
+    }
+    else   {
+      textarea = $("<textarea class='form-control bg-secondary text-white'>");
+
+    }
+    textarea.attr("id", "textarea" + index)
+
+    var getTextareaValue = localStorage.getItem("textarea" + index);
+    textarea.text(getTextareaValue);
+
     col2.append(textarea)
     var col3 = $("<div class='col-sm-2'>")
     var saveBtn = $("<button class='btn btn-primary saveBtn'>")
@@ -40,25 +67,27 @@ function showPlanner() {
 }
 showPlanner()
 
+$(".saveBtn").on("click", function() {
+      for (let index = 0; index < hourValues.length; index++) {
+               var getTextareaValue = $("#textarea" + index).val();
+               localStorage.setItem("textarea" + index, getTextareaValue);
+          
+               
+      }
+})
 
-//default color (time passed)
-$("textarea.form-control").css("background-color","lightgrey");
+// //default color (time passed)
+// $("textarea.form-control").css("background-color","lightgrey");
 
-function futureApp() {
-  if (currentHour <= hourValues.length[index]) {
-    $("textarea.form-control").css("background-color","green");
-  }
+// function futureApp() {
 
-}
+//   var hourValues = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 1, 2, 3, 4, 5, 6, 7, 8];
+  
+//   if (currentHour <= hourValues.length[index]) {
+//     $("textarea.form-control").css("background-color","green");
+//   }
 
-$("saveBtn").on("click", "textarea", function(){
-  var test = localStorage.getItem("test");
-  var obj = [];
-  if(test){
-      obj= JSON.parse(test);  
-  }
-  obj.push({"id":  id, "name":name});
-  localStorage.setItem("test",JSON.stringify(obj));
-});
+// }
+
 
 
